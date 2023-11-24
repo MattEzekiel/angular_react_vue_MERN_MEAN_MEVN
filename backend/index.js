8da87e3ce583e44008c69c68847df990368ea176
Matias Bubello <matt.bubello@gmail.com>
2023-11-24 18:29:18 -0300

    dotenv

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,11 @@
 'use strict';
+require('dotenv').config();
 const mongoose = require('mongoose');
 const app = require('./app');
 const port = 4000;
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb+srv://root:root@framework.tauzskr.mongodb.net/', {useNewUrlParser: true})
+mongoose.connect(process.env.MONGO_DB, {useNewUrlParser: true})
     .then(() => {
         console.log('MongoDB connected successfully');
         app.listen(port, () => {
